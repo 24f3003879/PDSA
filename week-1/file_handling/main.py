c1d68fae579cda18377eb0e8cf0f5ca6a787e669
Shubham <shubhamsingh705010@gmail.com>
2026-06-29 11:46:11 +0530

    reading and writing in file

---
+++ week-1/file_handling/main.py
@@ -0,0 +1,22 @@
+# opening and reading a file
+
+f = open('week-1/file_handling/example.txt', 'r')
+for line in f:
+    print(line, end='')
+f.close()
+
+# creating a file and writing in it
+
+w = open('week-1/file_handling/write.txt', 'w')
+w.write('Shubh')
+w.write('\n')
+w.write('Ankit')
+w.write('\n')
+w.write('Rajni')
+w.write('\n')
+w.write('Ishu')
+w.write('\n')
+w.write('Samar')
+w.close()
+
+
